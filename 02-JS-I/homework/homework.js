68b849b728df32df68ab160955770dbf6c562efb
Matías Biott <matias@biott.com.ar>
2022-03-11 21:31:33 -0300

    completed homework.js 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -201,7 +201,7 @@ function retornarPerimetro(lado){
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-return base * altura / 2;
+  return base * altura / 2;
 }
 
 
